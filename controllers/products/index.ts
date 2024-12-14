5a1c1d29ecf38cb2209a9cbd37de5eb0e9e6ae2d
eliguy343 <eliguy343@gmail.com>
2024-12-14 20:31:37 +0200

    controller

---
+++ controllers/products/index.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+
+export async function  getProducts(req:Request, res: Response) {
+  try {
+    
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
+}
