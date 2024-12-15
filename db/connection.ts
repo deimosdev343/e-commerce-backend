30461561e99695dc532f1f40d7dff7fc937118de
Eli Guy <eliguy343@gmail.com>
2024-12-15 15:52:48 +0200

    DB connection

--- db/connection.ts
+++ db/connection.ts
@@ -1,9 +1,12 @@
 import {connect} from 'mongoose';
 
 export const connection = async () => {
-  try {
-    
-  } catch (err) {
-    
-  }
+    if(!process.env.DB) {
+      throw Error("No DB URI Provided");
+    }
+    if(process.env.DB) {
+      console.log(process.env.DB);
+      await connect(process.env.DB);
+      console.log("DB Sucessfully Connected");
+    }
 }
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 import ProductRouter from "./routes/products/ProductRouter";
+import { connection } from "./db/connection";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 app.use("/products", ProductRouter);
+await connection();
 
 app.listen(port, () => {
   console.log(`Started Backend on port ${port}`);
