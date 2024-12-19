dfa395b2b940565a27a6a5daf43af29cd9533e75
eliguy343 <eliguy343@gmail.com>
2024-12-19 22:47:07 +0200

    update prod

--- controllers/products/index.ts
+++ controllers/products/index.ts
@@ -46,7 +46,25 @@ export async function createProduct(req: Request, res: Response): Promise<any> {
 
 export async function editProduct(req: Request, res: Response):Promise<any> {
   try {
-    res.status(200).json({msg:"Test"});
+    const {
+      id,
+      name, 
+      description, 
+      image,
+      price
+    } = req.body;
+    const product = await Product.findById(id);
+    if(!product) {
+      return res.status(401).json({msg:"Product Not Found"});
+    }
+    await Product.findByIdAndUpdate(id, {$set: {
+      name,
+      description,
+      image,
+      price
+    }});
+
+    res.status(200).json({msg:"Product successfully updated"});
   } catch (err) {
     console.log(err);
     res.status(500).json("Internal server Error");
