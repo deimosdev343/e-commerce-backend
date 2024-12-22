3a28dcdf8db4ed5f192656c096206d100aa86601
eliguy343 <eliguy343@gmail.com>
2024-12-22 22:33:50 +0200

    register user

---
+++ controllers/auth/index.ts
@@ -0,0 +1,22 @@
+import { Router, Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
+import User from '../../Models/User';
+
+
+export const registerUser = async (req: Request, res: Response) : Promise<any> => {
+  try {
+    const {email, password, name, address} = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      email,
+      password:hashedPassword,
+      name,
+      address
+    });
+    await user.save();
+    return res.status(201).json({msg: "User Successfully created"});
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({msg:"You can't change current reality and the zone kick you out"});
+  }
+} 
