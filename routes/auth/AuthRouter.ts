e126cc16a4d2d7d41d58ea7db1f8f0f67dbc99a2
eliguy343 <eliguy343@gmail.com>
2024-12-23 23:05:14 +0200

    register user

---
+++ routes/auth/AuthRouter.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import { validateData, userValidation } from '../../validation/validation';
+import { registerUser } from '../../controllers/auth/index';
+const AuthRouter = Router();
+AuthRouter.post('/register', validateData(userValidation), registerUser);
+
+export default AuthRouter;
