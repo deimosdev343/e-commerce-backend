2965b51021c69d76ac8565005f6921f5edce801d
eliguy343 <eliguy343@gmail.com>
2025-01-11 23:32:17 +0200

    product colors and sizes

--- Models/Product.ts
+++ Models/Product.ts
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 export interface IProduct {
-  name: string;
-  description: string;
-  image: string;
-  price: Number;
-  category: string;
+  name: string,
+  description: string,
+  image: string,
+  price: Number,
+  category: string,
+  colors: string[],
+  sizes: string[]
 }
 
 const productSchema = new mongoose.Schema<IProduct>({
@@ -13,7 +15,9 @@ const productSchema = new mongoose.Schema<IProduct>({
   description: {type: String, required: true},
   image: {type: String},
   price: {type: Number, required: true},
-  category: {type: String, required: true}
+  category: {type: String, required: true},
+  sizes: {type: [{type: String}]},
+  colors: {type: [{type: String}]}
 }, {timestamps: true});
 
 export default mongoose.model<IProduct>("product",productSchema) 
