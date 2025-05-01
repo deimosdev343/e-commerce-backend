398d362993a2a39e37115d95ecc7deb47bfdfe59
eliguy343 <eliguy343@gmail.com>
2025-05-01 22:20:26 +0300

    stat model

--- Models/Stat.ts
+++ Models/Stat.ts
@@ -1,9 +1,24 @@
 import mongoose from "mongoose";
 
 
-export interface IProduct {
+export interface IStat {
   type: "view" | "purchase",
   productId: string,
   purchaseId?: string,
-  
 }
+
+const statSchema = new mongoose.Schema<IStat>({
+  type: {
+    type: String,
+    required: true
+  },
+  productId: {
+    type: String,
+    required: true
+  },
+  purchaseId: {
+    type: String
+  }
+})
+
+export default mongoose.model<IStat>("Stat", statSchema);
