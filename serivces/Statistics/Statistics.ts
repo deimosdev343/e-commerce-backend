855e4f136f4422fbd2046f5532d871df9da47841
eliguy343 <eliguy343@gmail.com>
2025-05-02 22:52:02 +0300

    create view stat

--- serivces/Statistics/Statistics.ts
+++ serivces/Statistics/Statistics.ts
@@ -1,3 +1,11 @@
-export const createViewStatistic = async () => {
-  
+import Stat, { IStat } from "../../Models/Stat";
+
+export const createViewStatistic = async (stat :IStat) => {
+  const newStat = new Stat({
+    type:"view",
+    productId:stat.productId,
+    purchaseId:"",
+    ip:stat.ip
+  });
+  await newStat.save();
 }
