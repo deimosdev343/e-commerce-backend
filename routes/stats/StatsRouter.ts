f3cf21c5a241df6afdce12d614cd694449dd0941
Eli Guy <eliguy343@gmail.com>
2025-08-01 18:57:12 +0300

    Viewdata route

--- routes/stats/StatsRouter.ts
+++ routes/stats/StatsRouter.ts
@@ -10,7 +10,7 @@ const StatsRouter = Router();
 
 StatsRouter.get('/latestViews', verifyToken, verifySeller, getLatestViews)
 StatsRouter.get('/latestPurchases', verifyToken, verifySeller, getLatestPurchases);
-// StatsRouter.get('/topViews',verifyToken, verifySeller, getMostViewedProduct);
+StatsRouter.get('/topViews',verifyToken, verifySeller, getMostViewedProduct);
 // StatsRouter.get('/topSells', verifyToken, verifySeller, get)
 
 export default StatsRouter;
