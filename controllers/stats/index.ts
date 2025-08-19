2659885a61c0e633808efba3a07b52f36d80b132
eliguy343 <eliguy343@gmail.com>
2025-08-19 23:42:40 +0300

    latest

--- controllers/stats/index.ts
+++ controllers/stats/index.ts
@@ -28,6 +28,17 @@ export const getLatestPurchases = async (req:Request, res:Response): Promise<any
   }
 }
 
+export const getLatestViewsAndPurchases = async (req:Request, res:Response): Promise<any> => {
+  try {
+    const orders = await Stat.find({type:"purchase"}).sort({updatedAt:-1}).limit(10).lean();
+    const views = await Stat.find({type:"view"}).sort({updatedAt: -1}).limit(10).lean();
+    
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({msg:"Unknown Error"});
+  }
+}
+
 export const getMostPurchasedProducts = async (req:Request, res: Response): Promise<any> => {
   try {
     const purchases = await ItemOrderCount.find({}).sort({amount: -1}).limit(5).lean();
@@ -92,5 +103,4 @@ export const getMostViewedProduct = async (req:Request, res:Response): Promise<a
     console.log(err);
     return res.status(500).json({msg:"Unknown Error"});
   }
-
 }
