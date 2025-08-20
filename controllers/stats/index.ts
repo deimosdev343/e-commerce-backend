4d328c73c008a100e36c2d9df3fa07a54970de16
eliguy343 <eliguy343@gmail.com>
2025-08-20 23:00:04 +0300

    gte month

--- controllers/stats/index.ts
+++ controllers/stats/index.ts
@@ -30,8 +30,24 @@ export const getLatestPurchases = async (req:Request, res:Response): Promise<any
 
 export const getLatestViewsAndPurchases = async (req:Request, res:Response): Promise<any> => {
   try {
-    const orders = await Stat.find({type:"purchase"}).sort({updatedAt:-1}).limit(10).lean();
-    const views = await Stat.find({type:"view"}).sort({updatedAt: -1}).limit(10).lean();
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth();
+    const monthsToSubtract = 1; // Example: subtract 3 months
+    currentDate.setMonth(currentMonth - monthsToSubtract);
+    const orders = await Stat.find({
+        type:"purchase",
+        updatedAt:{$gte: currentDate.toISOString()}
+      })
+      .sort({updatedAt:-1})
+      .limit(10)
+      .lean();
+    const views = await Stat.find({
+        type:"view",
+        updatedAt:{$gte: currentDate.toISOString()}
+      })
+      .sort({updatedAt: -1})
+      .limit(10)
+      .lean();
     
   } catch (err) {
     console.log(err);
