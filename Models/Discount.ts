ecdf78183e4de31f04913dc6c135f783f3e531bf
Eli Guy <eliguy343@gmail.com>
2025-11-06 19:22:21 +0200

    Discount

--- Models/Discount.ts
+++ Models/Discount.ts
@@ -1,3 +1,5 @@
+import mongoose from "mongoose"
+
 export interface IDiscount {
   productIds: string[],
   description: string,
@@ -6,3 +8,33 @@ export interface IDiscount {
   startDate: Date,
   endDate: Date
 }
+
+const discountSchema = new mongoose.Schema<IDiscount>({
+  productIds: {
+    type: [{
+      type: String
+    }]
+  },
+  description:{
+    type: String,
+    required: true
+  },
+  image:{
+    type: String,
+    required: true
+  },
+  discountAmount: {
+    type: Number,
+    required: true
+  },
+  startDate: {
+    type: Date,
+    required: true
+  },
+  endDate: {
+    type: Date,
+    required: true
+  }
+});
+
+export default mongoose.model<IDiscount>("Discount", discountSchema);
